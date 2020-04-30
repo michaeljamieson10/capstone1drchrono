946716e69a605639d1dbe9d9b2a00295e3d10717
michael <michael@michaels-MBP.fios-router.home>
2020-04-29 20:26:55 -0400

    added header to response and changed data to no payload

--- app.py
+++ app.py
@@ -17,10 +17,9 @@ def authorization():
     """Show homepage."""
     redirect_uri = 'http://127.0.0.1:5000/token'
     client_id = 'IvnxhWMo16B9BQBVs89XfvEWYgDrPch5ZGnwCBvK'
-    # secret = ''
-    url = f"https://drchrono.com/o/authorize/?redirect_uri={redirect_uri}&response_type=code&client_id={client_id}"
+    url = f"https://drchrono.com/o/authorize/?redirect_uri={redirect_uri}&response_type=code&client_id={client_id}&scope=user:write user:read user patients:read patients:write"
     res = requests.get(url)
-    return redirect(f"{url}")
+    return redirect(url)
 
 @app.route("/token", methods=["GET", "POST"])
 def token():
@@ -40,29 +39,38 @@ def token():
     # Save these in your database associated with the user
     session['access_token'] =  data['access_token']
     session['refresh_token'] =  data['refresh_token']
-
-    response_two = requests.post('https://drchrono.com/o/token/', data={
-    'refresh_token': session['refresh_token'],
-    'grant_type': session['refresh_token'],
-    'client_id': 'IvnxhWMo16B9BQBVs89XfvEWYgDrPch5ZGnwCBvK',
-    'client_secret': 'BHHd5xFV1arUnXykMl5CRiSKu8q6GFNCEcgKVyYMZktalOo2VE3dZlG8z94QJoaUT9RuMCf8OWehrKz0Vj4ShrXLmA6GHCBwGUJdqP0dUiJbf9xKhAVwuDEltTz4n3qw',
-})
     return redirect("/")
 
 @app.route("/doctor")
 def list_doctor():
     """Will list doctors"""
-
     # This returns a 401
 
-    # import pdb; pdb.set_trace()
-    response = requests.get('https://app.drchrono.com/api/doctors', data={
-        'access_token': session['access_token'],
-    })
+    response = requests.get('https://app.drchrono.com/api/doctors')
     response.raise_for_status()
-    print(session['access_token'])
     raise
-    return jsonify(response)
+    return redirect('/')
+
+
+@app.route("/user")
+def user():
+    """Will list current user"""
+    # This returns a 401
+    url = "https://drchrono.com/api/users/current"
+
+    payload = {}
+    headers = {
+    'Authorization': 'Bearer your_access_token_here',
+    }
+
+    response = requests.request("GET", url, headers=headers, data = payload)
+
+    print(response.text.encode('utf8'))
+
+    response.raise_for_status()
+    raise
+    return redirect('/')
+    # response = requests.get('https://drchrono.com/api/users/current')
 
 @app.route("/patients")
 def list_patients():
@@ -71,11 +79,8 @@ def list_patients():
     # This returns a 401
 
     # import pdb; pdb.set_trace()
-    response = requests.get('https://app.drchrono.com/api/patients', data={
-        'access_token': session['access_token'],
-    })
+    response = requests.get('https://app.drchrono.com/api/patients')
     response.raise_for_status()
-    print(session['access_token'])
     raise
     return jsonify(response)
 
@@ -86,7 +91,6 @@ def create_pt():
     # This returns a 401
 
     response = requests.post('https://app.drchrono.com/api/patients', data={
-        'token': session['access_token'],
         'chart_id': '1',
         'date_of_birth': '03/31/1995',
         'doctor': 1,
