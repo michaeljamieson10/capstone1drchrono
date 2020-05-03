b740c30fc1637ee5657e2a099f9e7ea70c191670
michael <michael@michaels-MBP.fios-router.home>
2020-05-02 20:41:47 -0400

    adding ajax axios to medication.html, have issues with CORS, not allowing a get request to api drchrono in api-classes

--- app.py
+++ app.py
@@ -1,31 +1,44 @@
 from flask import Flask, render_template, request, redirect, jsonify, make_response, session
 import requests
-import random
-
+import datetime
+import json
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "SSHH SECRETO"
+
+def header_create():
+    access_token = session['access_token']
+    headers={
+    'Authorization':'Bearer %s' %access_token,
+    'Content-type':"application/json"
+    }
+    return headers
+
+
 
 @app.route("/", methods=["GET", "POST"])
 def homepage():
     """Show homepage."""
     
     return render_template("index.html")
+@app.route("/login", methods=["GET", "POST"])
+def loginpage():
+    """Show homepage."""
+    
+    return render_template("login.html")
 
 @app.route("/authorized", methods=["POST"])
 def authorization():
     """Show homepage."""
     redirect_uri = 'http://127.0.0.1:5000/token'
     client_id = 'IvnxhWMo16B9BQBVs89XfvEWYgDrPch5ZGnwCBvK'
-    url = f"https://drchrono.com/o/authorize/?redirect_uri={redirect_uri}&response_type=code&client_id={client_id}&scope=user:write user:read user patients:read patients:write"
+    url = f"https://drchrono.com/o/authorize/?redirect_uri={redirect_uri}&response_type=code&client_id={client_id}"
     res = requests.get(url)
     return redirect(url)
 
 @app.route("/token", methods=["GET", "POST"])
 def token():
     """redirec to token page."""
-    # if 'error' in get_params:
-        # raise ValueError('Error authorizing application: %s' % get_params[error])
 
     response = requests.post('https://drchrono.com/o/token/', data={
         'code': request.args.get('code'),
@@ -44,10 +57,10 @@ def token():
 @app.route("/doctor")
 def list_doctor():
     """Will list doctors"""
-    # This returns a 401
-
-    response = requests.get('https://app.drchrono.com/api/doctors')
+    url = "https://app.drchrono.com/api/doctors"
+    response = requests.get(url, headers=header_create())
     response.raise_for_status()
+    data = response.json()
     raise
     return redirect('/')
 
@@ -55,57 +68,63 @@ def list_doctor():
 @app.route("/user")
 def user():
     """Will list current user"""
-    # This returns a 401
-    access_token = session['access_token']
     url = "https://drchrono.com/api/users/current"
-
-    payload = {}
-    headers = {
-    'Authorization': access_token,
-    }
-    # raise
-    response = requests.request("GET", url, headers=headers, data = payload)
-
-    print(response.text.encode('utf8'))
-
+    response = requests.get(url, headers=header_create())
     response.raise_for_status()
-    raise
+    data = response.json()
     return redirect('/')
-    # response = requests.get('https://drchrono.com/api/users/current')
 
 @app.route("/patients")
 def list_patients():
     """Will list patients"""
-
-    # This returns a 401
-
-    # import pdb; pdb.set_trace()
-    response = requests.get('https://app.drchrono.com/api/patients')
+    url = "https://app.drchrono.com/api/patients"
+    
+    response = requests.get(url, headers=header_create())
     response.raise_for_status()
-    raise
-    return jsonify(response)
+    data = response.json()
+    return render_template("patients.html", data=data['results'])
 
+@app.route("/medications")
+def list_medications():
+    """Will list medications with patients in side bar menu"""
+    url = "https://app.drchrono.com/api/medications"
+    body = {
+        'patient' : 87214300
+    }
+    response = requests.get(url, headers=header_create(),data=json.dumps(body))
+    response.raise_for_status()
+    data = response.json()
+    url = "https://app.drchrono.com/api/patients"
+    
+    response = requests.get(url, headers=header_create())
+    response.raise_for_status()
+    patient_data = response.json()
+    return render_template("medications.html",data=data['results'], patient_data=patient_data['results'], access_token=session['access_token'])
+
+@app.route("/patients/<patient_id>")
+def list_patient(patient_id):
+    """Will list patients"""
+    url = f"https://app.drchrono.com/api/patients/{patient_id}"
+    
+    response = requests.get(url, headers=header_create())
+    response.raise_for_status()
+    data = response.json()
+    return render_template("users/detail.html", data=data['results'])
 @app.route("/patient_create")
 def create_pt():
     """Will create patients """
+    url = "https://app.drchrono.com/api/patients"
+    body = {
+        'doctor': 266342,
+        'first_name': 'Michaelas',
+        'last_name': 'Jamiesonas',
+        'date_of_birth': '1995-03-31',
+        'gender': 'Male'
+    }
 
-    # This returns a 401
-
-    response = requests.post('https://app.drchrono.com/api/patients', data={
-        'chart_id': '1',
-        'date_of_birth': '03/31/1995',
-        'doctor': 1,
-        'email': 'j@gmail.com',
-        'ethnicity': 'Asian',
-        'first_name': 'Michael',
-        'gender': 'Male',
-        'last_name': 'Jamieson',
-        'preferred_langage': 'English',
-        'race': 'Italian',
-        'since': '4/28/20',
-    })
-    response.content
-    raise
+    response = requests.post(url,headers=header_create(), data=json.dumps(body))
+    response.raise_for_status()
+    data = response.json()
     return response.json()
 
     
--- static/api-classes.js
+++ static/api-classes.js
@@ -1,1 +1,18 @@
-// 
+const BASE_URL = "https://app.drchrono.com/api";
+
+async function getPatientId(patient) {
+  
+  const headers = {
+    'headers':{
+      'Authorization':`Bearer ${access_token}`, 
+      'Content-type':"application/json",
+      'Access-Control-Allow-Origin': 'http://127.0.0.1:5000',
+      'Access-Control-Allow-Credentials': true
+      }
+  }
+  const response = await axios.get(`${BASE_URL}/medications`,{
+    patient: patient
+  },headers)
+  // patient is patient_id
+  console.log(response)
+};
